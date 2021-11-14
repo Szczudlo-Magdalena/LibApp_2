ef083ce4876a53ca3192d7f2f3cdd012cb9dd20d
Szczudlo-Magdalena <lyzwinska.m@gmail.com>
2021-11-14 15:43:43 +0100

    Added MembershipType model

---
+++ Models/MembershipType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp_Gr2.Models
+{
+    public class MembershipType
+    {
+    }
+}
