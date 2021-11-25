fd21d34666fbb05ea7de50e0e668f3c7434b3d8c
Szczudlo-Magdalena <lyzwinska.m@gmail.com>
2021-11-25 19:53:05 +0100

    Added migration AddNameToMembershipType

---
+++ Data/Migrations/20211125185138_AddNameToMemebershipType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibApp_Gr2.Data.Migrations
+{
+    public partial class AddNameToMemebershipType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MembershipTypes",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MembershipTypes");
+        }
+    }
+}
